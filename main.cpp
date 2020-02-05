e3c73ea129fb44c277c75e0c0d2b0fef10bc9287
lthornton1 <35785743+lthornton1@users.noreply.github.com>
2020-02-05 11:47:01 -0800

    fix main

--- main.cpp
+++ main.cpp
@@ -6,20 +6,9 @@ using std::endl;
 
 int main()
 {
-<<<<<<< HEAD
 	int i;
 	int j;
 	cout<<"Hi, please enter two numbers: ";
 	std::cin>>i>>j;
 	return 0;
-=======
-    cout<<"Hi, please enter two numbers: ";
-
-    int x,y;
-
-    cin >> x >> y;
-    cout << x + y << endl;
-
-    return 0;
->>>>>>> 36d751d7996179b963cfbdfc3c858bfa839a607e
 }
